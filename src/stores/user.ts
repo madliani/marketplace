10fee5ff9a5d8bb36a7f2f7e3fc71f79f98ed63c
madliani <madliani@hotmail.com>
2024-03-16 17:03:20 +0400

    feat: user store added

---
+++ src/stores/user.ts
@@ -0,0 +1,67 @@
+import type { BackendUser, User } from '@/types/user'
+import { defineStore } from 'pinia'
+
+type Id = 'user'
+
+type State = {
+  user: User | null
+}
+
+type Getters = {}
+
+type Actions = Readonly<{
+  fetchUser: (id: User['id']) => void
+  clear: () => void
+}>
+
+/** Getting a random integer between two values. */
+const getRandomNumber = (min: number, max: number) => {
+  return Math.random() * (max - min) + min
+}
+
+/** Backend user validating. */
+const isValidUser = (user: BackendUser) => {
+  return (
+    typeof user.firstName === 'string' &&
+    typeof user.id === 'string' &&
+    typeof user.image === 'string' &&
+    typeof user.lastName === 'string'
+  )
+}
+
+/** User default value. */
+const user: User | null = null
+
+export const useUserStore = defineStore<Id, State, Getters, Actions>('user', {
+  state: () => ({
+    user
+  }),
+  actions: {
+    async fetchUser(id) {
+      try {
+        const userResponse = await fetch(`https://dummyjson.com/users/${id}`)
+        const userJson = await userResponse.json()
+
+        if (userJson) {
+          const user = userJson as unknown as BackendUser
+
+          if (isValidUser(user)) {
+            this.user = {
+              avatar: user.image,
+              balance: getRandomNumber(0, 1_000),
+              firstName: user.firstName,
+              id: user.id,
+              lastName: user.lastName
+            }
+          }
+        }
+      } catch (exception: unknown) {
+        console.error(exception)
+      }
+    },
+    clear() {
+      this.user = user
+    }
+  },
+  persist: true
+})
---
+++ src/types/user.ts
@@ -0,0 +1,14 @@
+export type BackendUser = {
+  firstName: string
+  id: string
+  image: string
+  lastName: string
+}
+
+export type User = {
+  avatar: string
+  balance: number
+  firstName: string
+  id: string
+  lastName: string
+}
