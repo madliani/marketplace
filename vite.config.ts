45a3af655bc71452a24de8a4d96830e3861b8aa4
madliani <madliani@hotmail.com>
2024-03-17 20:24:11 +0400

    fix: vite config changed

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 export default defineConfig({
   plugins: [vue(), VueDevTools()],
   server: {
+    host: true,
     port: 8080
   },
   resolve: {
