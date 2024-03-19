fdb397c9eb1444594e94e7d23fe028c31995e8e1
madliani <madliani@hotmail.com>
2024-03-19 22:38:54 +0400

    refactor: router refactored

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,22 +1,26 @@
 import { Routes } from '@/types/routes'
 import HomeView from '@/views/HomeView.vue'
 import ProductsView from '@/views/ProductsView.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type RouteRecordRaw, type RouterHistory } from 'vue-router'
+
+const history: Readonly<RouterHistory> = createWebHistory(import.meta.env.BASE_URL)
+
+const routes: Readonly<RouteRecordRaw[]> = [
+  {
+    component: HomeView,
+    name: 'home',
+    path: Routes.HOME
+  },
+  {
+    component: ProductsView,
+    name: 'products',
+    path: Routes.PRODUCTS
+  }
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      component: HomeView,
-      name: 'home',
-      path: Routes.HOME
-    },
-    {
-      component: ProductsView,
-      name: 'products',
-      path: Routes.PRODUCTS
-    }
-  ]
+  history,
+  routes
 })
 
 const goto = (path: Readonly<Routes>) => () => router.push(path)
