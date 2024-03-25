d9178381a853f6a3091d96279a41adaca652055e
madliani <madliani@hotmail.com>
2024-03-25 15:15:36 +0400

    refactor: shopping cart to cart list renamed

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -3,12 +3,12 @@
     <template #content>
       <span class="text-h4 mb-8">Shopping cart</span>
 
-      <ShoppingCart />
+      <CartList />
     </template>
   </MainLayout>
 </template>
 
 <script lang="ts" setup>
-import ShoppingCart from '@/components/ShoppingCart.vue'
+import CartList from '@/components/CartList.vue'
 import MainLayout from '@/layouts/MainLayout.vue'
 </script>
