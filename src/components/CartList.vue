61d85767b59abdd21e3d8364c49a6cc1d2e32002
madliani <madliani@hotmail.com>
2024-03-27 03:34:17 +0400

    fix: shopping cart fixed

--- src/components/CartList.vue
+++ src/components/CartList.vue
@@ -1,37 +1,31 @@
 <template>
-  <template v-if="shoppingCart.length">
-    <v-list class="mx-4 mb-4">
-      <template v-for="item in shoppingCart" :key="item.id">
-        <CartItem :item="item" />
+  <v-list class="mx-4 mb-4">
+    <template v-for="item in shoppingCart" :key="item.id">
+      <CartItem :item="item" />
 
-        <v-divider class="mb-2" />
-      </template>
+      <v-divider class="mb-2" />
+    </template>
 
-      <v-list-item :title="`Total price: ${totalCost} $`" class="text-right" />
-    </v-list>
+    <v-list-item :title="`Total price: ${totalCost} $`" class="text-right" />
+  </v-list>
 
-    <AlertError
-      v-if="error"
-      class="mb-4"
-      title="Not enough funds!"
-      text="The total cost of the items in the shopping cart is greater than the balance."
-      :on-close="handleCloseClick"
-    />
+  <AlertError
+    v-if="error"
+    class="mb-4"
+    title="Not enough funds!"
+    text="The total cost of the items in the shopping cart is greater than the balance."
+    :on-close="handleCloseClick"
+  />
 
-    <v-btn-group>
-      <v-btn color="primary" title="Place an order" variant="outlined" @click="handlePlaceClick"
-        >Place an order</v-btn
-      >
+  <v-btn-group>
+    <v-btn color="primary" title="Place an order" variant="outlined" @click="handlePlaceClick"
+      >Place an order</v-btn
+    >
 
-      <v-btn color="secondary" title="Empty a cart" variant="outlined" @click="handleClear"
-        >Empty a cart</v-btn
-      >
-    </v-btn-group>
-  </template>
-
-  <v-card v-if="!shoppingCart.length" type="info" variant="elevated">
-    <v-alert text="There are no items in cart." title="Cart is empty!" variant="elevated" />
-  </v-card>
+    <v-btn color="secondary" title="Empty a cart" variant="outlined" @click="handleClear"
+      >Empty a cart</v-btn
+    >
+  </v-btn-group>
 </template>
 
 <script lang="ts" setup>
