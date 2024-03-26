b15e631e666f534ba709647fe7ddb457f9f0a7cd
madliani <madliani@hotmail.com>
2024-03-27 03:46:02 +0400

    fix: purchase order fixed

--- src/components/OrderTable.vue
+++ src/components/OrderTable.vue
@@ -1,5 +1,5 @@
 <template>
-  <template v-if="purchaseOrder && purchaseOrder.buyer && !success">
+  <template v-if="purchaseOrder && purchaseOrder.buyer && purchaseOrder.departureDate">
     <v-table class="w-75 mb-2" fixed-header>
       <caption class="text-h6">
         Item list
@@ -12,6 +12,8 @@
           <th class="text-left">Price</th>
           <th class="text-left">Quantity</th>
           <th class="text-left">Cost</th>
+          <th class="text-left">Buyer</th>
+          <th class="text-left">Departure date</th>
         </tr>
       </thead>
 
@@ -22,33 +24,13 @@
           <td>{{ item.product.price }}</td>
           <td>{{ item.quantity }}</td>
           <td>{{ item.cost }} &dollar;</td>
+          <td>{{ purchaseOrder.buyer.firstName }} {{ purchaseOrder.buyer.lastName }}</td>
+          <td>{{ purchaseOrder.departureDate.toDateString() }}</td>
         </tr>
       </tbody>
     </v-table>
 
     <span class="text-right w-75 mb-4">Total cost: {{ purchaseOrder.totalCost }} &dollar;</span>
-
-    <v-table class="w-75 mb-4" fixed-header>
-      <caption class="text-h6">
-        Buyer
-      </caption>
-
-      <thead>
-        <tr>
-          <th class="text-left">#</th>
-          <th class="text-left">First name</th>
-          <th class="text-left">Last name</th>
-        </tr>
-      </thead>
-
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>{{ purchaseOrder.buyer.firstName }}</td>
-          <td>{{ purchaseOrder.buyer.lastName }}</td>
-        </tr>
-      </tbody>
-    </v-table>
   </template>
 
   <AlertSuccess
