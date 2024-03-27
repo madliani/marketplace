d3aa0d8e38627b35d9525acdc788223c65731ba6
madliani <madliani@hotmail.com>
2024-03-27 22:49:28 +0400

    fix: shopping cart item fixed

--- src/components/CartItem.vue
+++ src/components/CartItem.vue
@@ -1,13 +1,24 @@
 <template>
-  <v-list-item
-    :prepend-avatar="item.product.thumbnail"
-    :subtitle="item.product.description"
-    :title="item.product.title"
-  >
-    <template #append>
+  <v-list-item>
+    <template #prepend>
+      <v-list-item-media class="mr-4">
+        <v-avatar
+          :image="item.product.thumbnail"
+          :alt="item.product.title"
+          :title="item.product.title"
+          size="48px"
+        />
+      </v-list-item-media>
+    </template>
+
+    <v-list-item-title class="mb-2">{{ item.product.title }}</v-list-item-title>
+
+    <v-list-item-subtitle class="mb-2">{{ item.product.description }}</v-list-item-subtitle>
+
+    <v-form class="d-inline-block mr-4 mb-2">
       <v-btn
         :disabled="item.quantity === 1 || !!purchaseOrder"
-        class="ml-4"
+        class="mr-2"
         color="secondary"
         title="Less"
         variant="tonal"
@@ -15,30 +26,29 @@
         >-</v-btn
       >
 
-      <span :title="`${item.quantity} piece`" class="ml-2">{{ item.quantity }} piece</span>
+      <span :title="`${item.quantity} piece`" class="mr-2">{{ item.quantity }} piece</span>
 
       <v-btn
         :disabled="!!purchaseOrder"
-        class="ml-2"
+        class="mr-2"
         color="secondary"
         title="More"
         variant="tonal"
         @click="handleMoreClick"
         >+</v-btn
       >
+    </v-form>
 
-      <span :title="`Price: ${item.cost} $`" class="ml-4">{{ item.cost }} &dollar;</span>
+    <span :title="`Price: ${item.cost} $`" class="mr-4">{{ item.cost }} &dollar;</span>
 
-      <v-btn
-        :disabled="!!purchaseOrder"
-        class="ml-4"
-        color="red"
-        icon="mdi-delete"
-        title="Delete"
-        variant="text"
-        @click="handleDeleteItem"
-      />
-    </template>
+    <v-btn
+      :disabled="!!purchaseOrder"
+      color="red"
+      icon="mdi-delete"
+      title="Delete"
+      variant="text"
+      @click="handleDeleteItem"
+    />
   </v-list-item>
 </template>
 
